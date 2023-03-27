f1dba135d3a3926f35390384e8a6d71c603a13e7
Kirubel <kirubel@ienetworks.co>
2023-03-27 21:25:29 +0300

    features page heading added

--- src/container/features/Features.jsx
+++ src/container/features/Features.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './features.css'
 
-const Features = ({title,text}) => {
+const Features = () => {
   return (
-    <div>
-      
+    <div className='gpt3__features section_padding' id='features'>
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+      </div>
     </div>
   )
 }
