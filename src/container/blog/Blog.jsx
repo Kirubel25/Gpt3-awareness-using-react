4ca8696bfd0c441d9175433b97063c62678fbbb2
Kirubel <kirubel@ienetworks.co>
2023-04-03 22:51:57 +0300

    final commit for the blog and the footer without risponsive view

--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
-import './blog.css'
+import React from "react";
+import "./blog.css";
+
+import { Article } from "../../components";
+import { blog1, blog2, blog3, blog4, blog5 } from "./blogimport";
 
 const Blog = () => {
   return (
-    <div className='gpt3__blog section__padding'>
+    <div className="gpt3__blog section__padding">
       <div className="gpt3__blog-header">
-        <h1 className="gradiant__text">A lot is happening, 
-We are blogging about it.</h1>
+        <h1 className="gradiant__text">
+          A lot is happening, We are blogging about it.
+        </h1>
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container__groupA">
-          Article
+          <Article
+            imgUrl={blog1}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
         </div>
         <div className="gpt3__blog-container__groupB">
-          Article
-          Article
-          Article
-          Article
+          <Article
+            imgUrl={blog2}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog3}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog4}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog5}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
